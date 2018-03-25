c147e6b9a8f169c20ef93c2e37bdbc96ac555fcf
Marten Terblanche <martenterblanche@gmail.com>
2018-03-25 15:10:56 +0200

    Updated main component to only show partials if data is available Moved getting api data call to parent component

--- src/reducers/weather-detail/days-weather/index.js
+++ src/reducers/weather-detail/days-weather/index.js
@@ -3,7 +3,7 @@ import { FETCH_DAYS_WEATHER } from '../../ui/actions';
 const INIT_STATE = {};
 
 export default function (state = INIT_STATE, action) {
-  console.log('state FETCH_DAYS_WEATHER', state);
+  // console.log('state FETCH_DAYS_WEATHER', state);
   // console.log('action FETCH_DAYS_WEATHER', action);
   if (action.error) {
     return action;
